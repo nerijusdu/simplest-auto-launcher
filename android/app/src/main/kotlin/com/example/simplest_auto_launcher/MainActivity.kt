bd7347bcbd3d2f3805c6a12be624a59922ab0df9
Nerijus <dnerijus72@gmail.com>
2022-04-20 23:51:14 +0300

    release prep

--- android/app/src/main/kotlin/com/example/simplest_auto_launcher/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.simplest_auto_launcher
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/lt/nerijusdulke/simplest_auto_launcher/MainActivity.kt
@@ -0,0 +1,6 @@
+package lt.nerijusdulke.simplest_auto_launcher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
